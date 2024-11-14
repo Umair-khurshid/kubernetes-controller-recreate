3a595c4bdecb916b78c9acc358d60b84cfe945c6
lhb <umairkhurshidshaheen@gmail.com>
2024-11-14 04:39:24 -0500

    Improved error handling and refactored cleanup function

--- controller-recreate.sh
+++ controller-recreate.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 set -eE
 
+# Check if arguments are provided
 if [[ $# != 2 ]]; then
-  echo Usage: $0 type controller
-  echo Example: $0 sts mon-sts
+  echo "Usage: $0 <type> <controller>"
+  echo "Example: $0 sts mon-sts"
   exit 1
 fi
 
 type=$1
 name=$2
 
-if ! kubectl get $type $name &>/dev/null ; then
-  echo Unable to retrieve info about the $type controller $name
+# Check if the Kubernetes controller exists
+if ! kubectl get "$type" "$name" &>/dev/null; then
+  echo "Unable to retrieve info about the $type controller $name."
   exit 1
 fi
 
+# Function to clean up and rollback
 function clean {
-  echo Rollbacking controller objects...
-  if [[ ! -z $STS ]]; then
-    echo $STS | kubectl apply -f -
+  echo "Rolling back controller objects..."
+  if [[ -n $STS ]]; then
+    echo "$STS" | kubectl apply -f -
   fi
 }
 
-STS=$(kubectl get $type $name -ojson)
+# Store the current state of the controller
+STS=$(kubectl get "$type" "$name" -ojson)
 
+# Set up the trap for clean-up on error or exit
 trap clean ERR SIGINT SIGQUIT EXIT
 
-kubectl delete $type $name --cascade=false
+# Delete the controller without deleting associated resources (pods)
+kubectl delete "$type" "$name" --cascade=false
 
-echo Controller deleted. You can modify any pod created by the $type $name
-echo Press any key when you are done to proceed and rollback controller $name
-read
+echo "Controller deleted. You can modify any pod created by the $type $name."
 
-echo Operation ended gracefully
+# Optionally, interact with the user or automate the rollback step
+echo "Press any key when you are done to proceed and rollback controller $name."
+read -r
+
+echo "Operation ended."
+
